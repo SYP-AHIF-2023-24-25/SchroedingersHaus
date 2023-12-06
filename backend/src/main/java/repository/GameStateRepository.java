32b5a2b611c3abb69f4327eb8d544998886a2333
Sophie Böhm <boehm.sophie05@gmail.com>
2023-12-06 22:33:14 +0100

    Added GameState and worked on persistence

---
+++ backend/src/main/java/repository/GameStateRepository.java
@@ -0,0 +1,25 @@
+package repository;
+
+import entity.GameState;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Model;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+@Model
+public class GameStateRepository {
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public GameState findById(final String id) {
+        return entityManager.find(GameState.class, id);
+    }
+
+    @Transactional
+    public void saveGameState(GameState gameState) {
+        entityManager.persist(gameState);
+    }
+}
